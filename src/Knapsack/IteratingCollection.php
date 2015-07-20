c995891eb293da1bed94eea19dc52ad1c8e06e8f
Dusan Kasan <dusan@kasan.sk>
2015-07-20 08:45:55 +0200

    Optimizing iterating collection

--- src/Knapsack/IteratingCollection.php
+++ src/Knapsack/IteratingCollection.php
@@ -2,27 +2,10 @@
 
 namespace Knapsack;
 
-use Knapsack\Callback\Callback;
 use Knapsack\Exceptions\NoMoreItems;
-use Traversable;
 
 class IteratingCollection extends MappedCollection
 {
-    /**
-     * @var Callback
-     */
-    private $followedCallback;
-
-    /**
-     * @param array|Traversable $input
-     * @param callable $followedCallback
-     */
-    public function __construct($input, callable $followedCallback)
-    {
-        parent::__construct($input, $followedCallback);
-        $this->followedCallback = new Callback($followedCallback);
-    }
-
     public function valid()
     {
         if (parent::valid()) {
